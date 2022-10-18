aba866c3df66dda3e80b0c5c5225f73a61e1e635
littlephillips <phillipkinuthia72@gmail.com>
2022-10-18 12:36:06 +0300

    control flow loops

--- looping.rb
+++ looping.rb
@@ -1,6 +1,16 @@
+  # use while or until loop
+  # outputs numbers starting at 10 and counting down 1
+  # after reaching 1, print out "Happy New Year!
+
 def happy_new_year
-  # your code here
-end
+  counter = 10
+    until counter == 0
+      puts counter
+      counter -= 1
+    end
+      puts "Happy New Year!"
+  end
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -15,10 +25,31 @@ def fizzbuzz(num)
   end
 end
 
+
+  # Write a method `fizzbuzz_printer` that prints the numbers from 1 to 100. 
+  # For multiples of three, print "Fizz" instead of the number and for the multiples
+  # of five print "Buzz". For numbers which are multiples of both three and five,
+  # print "FizzBuzz".
+
 def fizzbuzz_printer
-  # your code here
+  (1..100).each do |num|
+    puts fizzbuzz num
+  end
 end
 
-def reverse_string(str)
-  # your code here
-end
+
+# one argument, a string & revereses it
+  # don't use the built in .reverse method
+  # loop thru the char's in the input string and reverse it
+    # i rep reversed
+
+def reverse_string str
+  split_str = str.split("")
+  i = []
+  str.size.times {i << split_str.pop} 
+  i.join
+end 
+
+
+
+
